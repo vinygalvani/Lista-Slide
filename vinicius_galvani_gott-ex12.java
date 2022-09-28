93cc3b1cf7f21c3149079fb5183bd93166d6b4c4
vinygalvani <92888841+vinygalvani@users.noreply.github.com>
2022-09-28 20:34:21 -0300

    Add files via upload

--- vinicius_galvani_gott-ex12.java
+++ vinicius_galvani_gott-ex12.java
@@ -7,18 +7,18 @@ class Main {
     
     float segundoNumero = Float.parseFloat(JOptionPane.showInputDialog("Digite o segundo número: "));
 
-    int choice = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma operação: \n[1]Somar. \n[2]Subtrair. \n[3]Multiplicar. \n[4]Dividir. \n[5]Sair."));
+    int escolha = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma operação: \n[1]Somar. \n[2]Subtrair. \n[3]Multiplicar. \n[4]Dividir. \n[5]Sair."));
 
-    if (choice == 1) {
+    if (escolha == 1) {
       JOptionPane.showMessageDialog(null, primeiroNumero + " + " + segundoNumero + " = " + (primeiroNumero+segundoNumero));
     }
-      else if (choice == 2) {
+      else if (escolha == 2) {
         JOptionPane.showMessageDialog(null, primeiroNumero + " - " + segundoNumero + " = " + (primeiroNumero-segundoNumero));
       }
-        else if (choice == 3) {
+        else if (escolha == 3) {
           JOptionPane.showMessageDialog(null, primeiroNumero + " * " + segundoNumero + " = " + (primeiroNumero*segundoNumero));
         }
-          else if (choice == 4) {
+          else if (escolha == 4) {
             JOptionPane.showMessageDialog(null, primeiroNumero + " / " + segundoNumero + " = " + (primeiroNumero/segundoNumero));
           }
           else {
